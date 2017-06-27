8bf79382b7e8c9c32259d41f49768ddd218c648f
PaulinBenoit <paulinb.benoit@hotmail.fr>
2017-06-27 17:54:32 +0200

    First upload sur github - master

---
+++ site_eeudf/views/acceuil.views.php
@@ -0,0 +1,117 @@
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Site du groupe local d'Annecy-Chambéry</title>
+
+    <link rel="stylesheet" href="../design/materialize.css">
+    <script src="../scripts/materialize.js"></script>
+    <script src="../scripts/jquery-3.2.1.js"></script>
+  </head>
+
+  <body>
+    <div class="container">
+
+      <!-- Panel de navigation -->
+      <div class="row">
+        <div class="col s12">
+          <nav>
+            <div class="nav-wrapper green">
+              <a href="#" class="brand-logo"><img src="../images/logo_eeudf.png" height="100%"></a>
+              <span style="margin-left:120px;font-size:25px">Site du groupe locale d'Annecy-Chambéry</span>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="acceuil.views.php">Présentation</a></li>
+                <li><a href="acceuil.views.php">Louveteaux</a></li>
+                <li><a href="acceuil.views.php">Eclaireurs</a></li>
+                <li><a href="acceuil.views.php">Aînés</a></li>
+                <li><a href="acceuil.views.php">Parents</a></li>
+                <li><a href="acceuil.views.php">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+      <!-- Header -->
+      <div class="row">
+
+      </div>
+
+      <div class="row">
+
+        <!-- Panel central gauche -->
+        <div class="col s8">
+          <div class="row">
+            <div class="col s12">
+              <div class="card amber lighten-1" style="text-align:center;"><h4>Dernières news</h4></div>
+
+              <div class="card horizontal amber lighten-3">
+                <div class="card-image"><img src="../images/template_image_actu.jpeg" height="200px"></div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in mi augue.
+                  Maecenas nec posuere odio, sed tincidunt ex. Quisque condimentum velit eget sapien venenatis posuere.
+                  In efficitur dolor sit amet tempus gravida. Etiam at odio varius, pretium orci auctor, ultricies leo.
+                  Curabitur rhoncus ullamcorper erat lacinia consequat. In metus.</p>
+                </div>
+              </div>
+
+              <div class="card horizontal amber lighten-3">
+                <div class="card-image"><img src="../images/template_image_actu.jpeg" height="200px"></div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in mi augue.
+                  Maecenas nec posuere odio, sed tincidunt ex. Quisque condimentum velit eget sapien venenatis posuere.
+                  In efficitur dolor sit amet tempus gravida. Etiam at odio varius, pretium orci auctor, ultricies leo.
+                  Curabitur rhoncus ullamcorper erat lacinia consequat. In metus.</p>
+                </div>
+              </div>
+
+              <div class="card horizontal amber lighten-3">
+                <div class="card-image"><img src="../images/template_image_actu.jpeg" height="200px"></div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in mi augue.
+                  Maecenas nec posuere odio, sed tincidunt ex. Quisque condimentum velit eget sapien venenatis posuere.
+                  In efficitur dolor sit amet tempus gravida. Etiam at odio varius, pretium orci auctor, ultricies leo.
+                  Curabitur rhoncus ullamcorper erat lacinia consequat. In metus.</p>
+                </div>
+              </div>
+
+              <div class="card horizontal amber lighten-3">
+                <div class="card-image"><img src="../images/template_image_actu.jpeg" height="200px"></div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in mi augue.
+                  Maecenas nec posuere odio, sed tincidunt ex. Quisque condimentum velit eget sapien venenatis posuere.
+                  In efficitur dolor sit amet tempus gravida. Etiam at odio varius, pretium orci auctor, ultricies leo.
+                  Curabitur rhoncus ullamcorper erat lacinia consequat. In metus.</p>
+                </div>
+              </div>
+
+              <div class="card horizontal amber lighten-3">
+                <div class="card-image"><img src="../images/template_image_actu.jpeg" height="200px"></div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in mi augue.
+                  Maecenas nec posuere odio, sed tincidunt ex. Quisque condimentum velit eget sapien venenatis posuere.
+                  In efficitur dolor sit amet tempus gravida. Etiam at odio varius, pretium orci auctor, ultricies leo.
+                  Curabitur rhoncus ullamcorper erat lacinia consequat. In metus.</p>
+                </div>
+              </div>
+
+              </div>
+            </div>
+          </div>
+
+          <!-- Panel central droit -->
+          <div class="col s4">
+            <div class="row">
+              <div class="col s12">
+                <div class="card red lighten-1" style="text-align:center;"><h4>Connexion</h4></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </body>
+</html>
